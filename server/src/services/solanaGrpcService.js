e3b1bed44f628bcec4f7311caa7b1fa937197c62
flakeed <flakeeed@gmail.com>
2025-09-23 18:12:14 +0300

    Try to fix

--- server/src/services/solanaGrpcService.js
+++ server/src/services/solanaGrpcService.js
@@ -34,7 +34,7 @@ class SolanaGrpcService {
 
         this.transactionBatch = new Map();
         this.batchTimer = null;
-        this.batchSize = 500; 
+        this.batchSize = 50; 
         this.batchTimeout = 10; 
 
         this.BUY_THRESHOLD = parseFloat(process.env.SOL_BUY_THRESHOLD) || 0.01;
